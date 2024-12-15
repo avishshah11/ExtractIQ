721b839a7581954c662ce7290c98369c10a702d0
avishshah11 <avishshah1108@gmail.com>
2024-12-15 23:43:47 +0530

    adding hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import HeroSection from "./components/HeroSection/HeroSection";
 import Header from "./components/NavBar/NavBar";
 import Search from "./components/Search/Search";
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Search/>
+      <Header />
+      <Search />
+      <HeroSection />
     </div>
   );
 }
---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,27 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Pagination } from "swiper/modules";
+
+const HeroSection = () => {
+  return (
+    <>
+      <Swiper
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+
+export default HeroSection;
